14f086ca10b09666eb4048a83d9dc07195895fd6
nbourre <nbourrereg@hotmail.com>
2020-02-12 14:00:21 -0500

    First!

---
+++ DataBinding_A_collections/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Model_used;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace DataBinding_A_collections
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Employee> Employees { get; private set; }
+
+        public MainWindow()
+        {
+            Employees = new List<Employee>()
+            {
+                new Employee() {Age = 45, LastName = "Simpson", Name="Homer", PicturePath = "images/homer.png"},
+                new Employee() {Age = 60, LastName = "Griffin", Name="Peter", PicturePath = "images/peter_griffin.png"},
+            };
+
+            InitializeComponent();
+        }
+
+        private void AddEmployee_click(object sender, RoutedEventArgs e)
+        {
+            var emp = new Employee() { Age = 52, LastName = "Gripson", Name = "Petmer", PicturePath = "images/Peter-Homer.jpg" };
+            Employees.Add(emp);
+
+        }
+    }
+}
---
+++ Model_used/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Model_used
+{
+    public class Employee : INotifyPropertyChanged
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string PicturePath { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
