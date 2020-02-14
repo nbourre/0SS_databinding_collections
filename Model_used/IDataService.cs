0e00018cb67eca691cdfa289949791f4eded1d90
nbourre <nbourrereg@hotmail.com>
2020-02-14 12:38:49 -0500

    Added the IDataService interface

---
+++ Model_used/IDataService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model_used
+{
+    public interface IDataService <T>
+    {
+        public IEnumerable<T> GetAll();
+    }
+}
